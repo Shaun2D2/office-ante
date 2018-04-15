5f9ff4935f3ef94334c3611a976acb4c3c68d1ca
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-04-15 07:38:33 -0700

    wip

--- client/react/Challenge/views/Stakes.jsx
+++ client/react/Challenge/views/Stakes.jsx
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
 
+const defaultStakes = [
+    {
+        title: 'Pie',
+        icon: 'fa-chart-pie'
+    },
+    {
+        title: 'Tacos',
+        icon: 'fa-lemon'
+    },
+    {
+        title: 'Lunch',
+        icon: 'fa-utensils'
+    }
+]
+
 class Stakes extends Component {
 
     render() {
         return (
-            
-            <div className="columns">
-                <div className="column">
-                    <div className="box text-center">
-                      <h1 className="is-size-3 has-text-centered">Pie</h1>
-                      <div className="has-text-centered">
-                          <i className="fa fa-chart-pie is-size-1" />
+            <div>
+              <h1 className="is-size-4">Select the stakes!</h1>
+              <div className="columns">
+                  {defaultStakes.map((stake) => (
+                    <div className="column">
+                        <div className="box text-center">
+                          <h1 className="is-size-3 has-text-centered">{stake.title}</h1>
+                          <div className="has-text-centered">
+                              <i className={`fa ${stake.icon} is-size-1`} />
+                          </div>
+                        </div>
+                    </div>
+                  ))}
+                  <div className="column">
+                      <div className="box">
+                        <h1 className="is-size-3 has-text-centered">Other</h1>
                       </div>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="box">
-                      2 Tacos
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="box">
-                      Tasty Lunch
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="box">
-                      Other
-                    </div>
-                </div>
+                  </div>
 
+              </div>
             </div>
         )
     }
